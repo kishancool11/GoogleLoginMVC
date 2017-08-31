9779a5442208be85b275d733d490a4d9c5a7b211
kishancool11 <kishancool11@gmail.com>
2017-08-31 07:55:37 -0700

    Update Startup.Auth.cs

--- CustomGoogleLogin/App_Start/Startup.Auth.cs
+++ CustomGoogleLogin/App_Start/Startup.Auth.cs
@@ -18,8 +18,8 @@ namespace CustomGoogleLogin
             });
             app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             {
-                ClientId = "857589842598-d29av6ftecn7bnnmhgplecafpt6tnfo0.apps.googleusercontent.com",
-                ClientSecret = "WQVQK_PDMesI_F-00e0U-G_S",
+                ClientId = "*******",
+                ClientSecret = "******",
                 CallbackPath = new PathString("/GoogleLoginCallback")
             });
         }
